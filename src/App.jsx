fe7413638aca0058a66b80f06ebcad166d82e2b1
Your Name <you@example.com>
2024-11-27 11:09:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <div className="appContainer">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
