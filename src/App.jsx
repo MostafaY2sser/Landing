bd14880ed915868ebb4d7cb5ea9a7a402f5ca1b1
Your Name <you@example.com>
2024-11-27 11:35:30 +0200

    last

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 
 
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <div className='app'>
-      <div className="appContainer bg-[red]">
-        ede
+      <div className="appContainer ">
+        <Header/>
       </div>
     </div>
   )
