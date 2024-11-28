299638af7389d8229f91fb89bf39996b6c539895
Your Name <you@example.com>
2024-11-28 13:52:45 +0200

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css'
 import Footer from './Components/Footer/Footer'
-import Header from './Components/Header/Header'
-
 import { FaWhatsapp } from "react-icons/fa";
-
 import { Routes, Route } from 'react-router-dom';
 import Chaina from "./Pages/Chaina/Chaina"
 import English from "./Pages/English/English"
@@ -12,7 +9,6 @@ import Franch from "./Pages/Franch/Franch"
 function App() {
   return (
     <div className='app'>
-        <Header/>
       <div className="appContainer ">
         <Routes>
           <Route path="/" element={<Chaina />} />
